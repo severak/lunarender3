825e86d3c0b78b301b155aff55a288ed38675b69
Severak <strajt9@seznam.cz>
2022-01-27 22:41:44 +0100

    WIP - tested mbtiles file.

--- src/mbtiles.nim
+++ src/mbtiles.nim
@@ -8,3 +8,14 @@ proc getTile*(db: DbConn, z: int, x: int, y: int): string =
 
 proc closeTiles*(db: DbConn): bool =
     db.close()
+
+when isMainModule:
+    import std/md5
+    import tile
+    var testdata = openTiles("src/testdata/lux.mbtiles")
+    discard decodeVectorTile(testdata.getTile(0,0,0))
+    discard decodeVectorTile(testdata.getTile(1,0,1))
+    discard decodeVectorTile(testdata.getTile(1,1,1))
+    discard decodeVectorTile(testdata.getTile(2,1,2))
+    discard testdata.closeTiles()
+    echo "OK"
