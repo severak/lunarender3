f88f5176306c884b976056d14e94aac6c9cf3ffc
Severak <strajt9@seznam.cz>
2022-01-27 20:31:09 +0100

    We have working server with debug layers.

--- lunarender3.nimble
+++ lunarender3.nimble
@@ -10,11 +10,14 @@ bin           = @["lunarender3"]
 
 # Dependencies
 
+# (versions installed on my machine, probably works even with lower versions)
+
 requires "nim >= 1.6.2"
 requires "jester >= 0.5.0"
 requires "pixie >= 3.1.2"
 requires "nimpb >= 0.2.0"
 requires "zippy >= 0.7.4"
+requires "cligen >= 1.5.20"
 
 task genparser, "Generates MVT parser.":
     exec "nimpb_build -I=src --out=src src/vector_tile.proto"
--- src/lunarender3.nim
+++ src/lunarender3.nim
@@ -1,10 +1,11 @@
-import jester
+import jester except server
 import pixie
+import cligen
 
-routes:
+router myrouter:
   get "/":
     resp "Lunarender v3 OK"
-  get "/@tileset/@zoom/@x/@y":
+  get "/@tileset/@zoom/@x/@y/debug.png":
     # TODO - conds aby nebyly parametry prazdny
     let image = newImage(256, 256)
     image.fill(rgba(255, 255, 255, 255))
@@ -12,6 +13,25 @@ routes:
     font.size = 20
 
     image.fillText(font.typeset(" x=" & @"x" & " y=" & @"y"), translate(vec2(10, 10)))
+    strokePath(image, "M 0 0 L 0 256 L 256 256 L 256 0 Z", "green".parseHtmlColor)
     resp encodeImage(image, ffPng), "image/png"
-    # resp "Tile " & @"tileset" & ": zoom=" & @"zoom" & " x=" & @"x" & " y=" & @"y" & "..."
-  
+  get "/@tileset/@zoom/@x/@y/debug512.png":
+    # TODO - conds aby nebyly parametry prazdny
+    let image = newImage(512, 512)
+    image.fill(rgba(255, 255, 255, 255))
+    var font = readFont("Gidole-Regular.ttf")
+    font.size = 20
+
+    image.fillText(font.typeset(" x=" & @"x" & " y=" & @"y"), translate(vec2(10, 10)))
+    strokePath(image, "M 0 0 L 0 512 L 512 512 L 512 0 Z", "green".parseHtmlColor)
+    resp encodeImage(image, ffPng), "image/png"
+
+proc server(data="") =
+  var jester = initJester(myrouter)
+  jester.serve()
+
+proc kek() =
+  echo "KEK"
+
+when isMainModule:
+  dispatchMulti([server], [kek])
